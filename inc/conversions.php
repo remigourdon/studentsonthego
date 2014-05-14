73c76b302807c59d8dc9c0132b42c8a37d64d142
remigourdon <contact@remigourdon.io>
2014-05-14 17:58:11 +0200

    The conversion from json to wkt now handles polygons and multipolygons.

--- inc/conversions.php
+++ inc/conversions.php
@@ -5,19 +5,32 @@
  */
 
 function json_to_wkt($json) {
-    $coordinates = json_decode($json)->{"features"}[0]->{"geometry"}->{"coordinates"};
+    $geometry = json_decode($json)->{"features"}[0]->{"geometry"};
+    $coordinates = $geometry->{"coordinates"};
+    $wkt = "";
 
-    $wkt = "MULTIPOLYGON(";
-
-    foreach ($coordinates as $polygon) {
-        $wkt .= "((";
-
-        // We take only the first linear ring
-        foreach ($polygon[0] as $point) {
-            $wkt .= "$point[0] $point[1],";
+    if($geometry->{"type"} == "Polygon") {
+        $wkt = "POLYGON(";
+        foreach ($coordinates as $linearRing) {
+            $wkt .= "(";
+            foreach($linearRing as $point) {
+                $wkt .= $point[0] . " " . $point[1] . ",";
+            }
+            $wkt = substr($wkt, 0, -1) . "),";
         }
-
-        $wkt = substr($wkt, 0, -1) . ")),";
+    } else if($geometry->{"type"} == "MultiPolygon") {
+        $wkt = "MULTIPOLYGON(";
+        foreach ($coordinates as $polygon) {
+            $wkt .= "(";
+            foreach ($polygon as $linearRing) {
+                $wkt .= "(";
+                foreach ($linearRing as $point) {
+                    $wkt .= $point[0] . " " . $point[1] . ",";
+                }
+                $wkt = substr($wkt, 0, -1) . "),";
+            }
+            $wkt = substr($wkt, 0, -1) . "),";
+        }
     }
 
     return substr($wkt, 0, -1) . ")";
