b429047da1ece8cd299e946c5586c51ec604d93c
remigourdon <contact@remigourdon.io>
2014-05-14 19:27:19 +0200

    Variable name is now countryName in edit-country.php.

--- edit-country.php
+++ edit-country.php
@@ -12,21 +12,21 @@ if(!isset($_GET['cid'])) {
     // There is data in POST, the form has been filled in
     if(!empty($_POST)) {
         $query = "";
-        $name   = isset($_POST['name']) ? $_POST['name'] : "";
+        $countryName   = isset($_POST['countryName']) ? $_POST['countryName'] : "";
 
-        if(!file_exists($_FILES['map']['tmp_name']) || $name == "") {
+        if(!file_exists($_FILES['map']['tmp_name']) || $countryName == "") {
             $content .= "<p>Please fill out all the fields.</p>";
-            $content .= getForm($name);
+            $content .= getForm($countryName);
         } else {
             if($_FILES['map']['error'] > 0 || $_FILES["map"]["type"] != "application/json") {
                 $content .= "<p>Please try again and verify that your file is JSON.</p>";
-                $content .= getForm($name);
+                $content .= getForm($countryName);
             } else {
                 include_once("inc/conversions.php");
                 include_once("inc/connstring.php");
 
                 // Avoid SQL injections and encode UTF-8 characters
-                $name = utf8_encode($mysqli->real_escape_string($name));
+                $countryName = utf8_encode($mysqli->real_escape_string($countryName));
 
                 $json = file_get_contents($_FILES['map']['tmp_name']);
 
@@ -37,7 +37,7 @@ if(!isset($_GET['cid'])) {
                 -- Inserts a new country into the database
                 --
                 INSERT INTO {$tableCountry}(countryName, countryMap)
-                VALUES('{$name}', GeomFromText('{$wkt}'));
+                VALUES('{$countryName}', GeomFromText('{$wkt}'));
 END;
                 // Performs query
                 $mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error);
@@ -45,14 +45,14 @@ END;
                 $mysqli->close();
 
                 // Informs the user of the success and invite him to send new data
-                $content .= "<p>$name has been successfully added to the database.</p>";
+                $content .= "<p>$countryName has been successfully added to the database.</p>";
                 $content .= getForm("");
             }
         }
     // POST is empty, the user has not filled in the form yet
     } else {
         $content .= "<p>Fill out the fields bellow to add a new country.</p>";
-        $content .= getForm($name);
+        $content .= getForm("");
     }
 } else {    // The country parameter is specified
     include_once("inc/connstring.php");
@@ -77,10 +77,10 @@ END;
     } else {
         // Fetch the data
         $row = $res->fetch_object();
-        $name = $row->countryName;
+        $countryName = $row->countryName;
 
-        $content .= "<p>Edit the data for $name.</p>";
-        $content .= getForm($name);
+        $content .= "<p>Edit the data for $countryName.</p>";
+        $content .= getForm($countryName);
     }
 
     $res->close();
@@ -89,13 +89,13 @@ END;
 
 echo $content;
 
-function getForm($name) {
-    $name = htmlspecialchars($name);
+function getForm($countryName) {
+    $countryName = htmlspecialchars($countryName);
 
     $html = <<<END
     <form action="edit.php" method="post" enctype="multipart/form-data">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" value="{$name}"><br>
+        <label for="countryName">Name:</label>
+        <input type="text" name="countryName" id="countryName" value="{$countryName}"><br>
         <label for="map">Map:</label>
         <input type="file" name="map" id="map"><br>
         <input type="submit" name="submit" value="Submit">
