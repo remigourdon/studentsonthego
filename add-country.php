5e0a4f591c7dfc5eedd66b1d84c44b43a84bedc2
remigourdon <contact@remigourdon.io>
2014-05-15 14:23:36 +0200

    Adding, modifying and deleting country is now split in 3 files.

---
+++ add-country.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This page allows an administrator to add a new country to the database.
+ */
+
+include_once("inc/HTMLTemplate.php");
+
+// Initializations
+$content = "";
+$tableCountries = "countries";
+
+// Display
+echo $header;
+echo $content;
+echo $footer;
+
+?>
---
+++ mod-country.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This page allows an administrator to modify a country in the database.
+ */
+
+include_once("inc/HTMLTemplate.php");
+
+// Initializations
+$content = "";
+$tableCountries = "countries";
+
+// Display
+echo $header;
+echo $content;
+echo $footer;
+
+?>
