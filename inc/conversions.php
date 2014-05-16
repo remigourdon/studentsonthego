99467d90d7e120fed66c810027b89790b3ce9972
remigourdon <contact@remigourdon.io>
2014-05-16 14:00:37 +0200

    Add new function to convert wkt data into geojson.

--- inc/conversions.php
+++ inc/conversions.php
@@ -4,6 +4,11 @@
  * Implements convenient methods for conversion of JSON and WKT.
  */
 
+/**
+ * Converts geojson into wkt (polygon or multipolygon).
+ * @param  string $json the geojson data
+ * @return string       the wkt data
+ */
 function json_to_wkt($json) {
     $geometry = json_decode($json)->{"features"}[0]->{"geometry"};
     $coordinates = $geometry->{"coordinates"};
@@ -36,8 +41,20 @@ function json_to_wkt($json) {
     return substr($wkt, 0, -1) . ")";
 }
 
+/**
+ * Converts wkt into geojson with properties.
+ * @param  array  $data the data as an array (wkt => properties[])
+ * @return string       the geojson data
+ */
+function wkt_to_json($data) {
 
+}
 
+/**
+ * Converts a wkt POINT into latitude and longitude.
+ * @param  string $wkt the wkt data
+ * @return array       the array containing latitude and longitude
+ */
 function point_to_coord($wkt) {
     $coord = explode("(", $wkt)[1];
     $coord = explode(")", $coord)[0];
