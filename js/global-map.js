1243222a25951f6a319d14893ffc46f0935e6044
remigourdon <contact@remigourdon.io>
2014-05-18 15:22:57 +0200

    Functional global map!

--- js/global-map.js
+++ js/global-map.js
@@ -1,28 +1,99 @@
-var width   = 700,
-    height  = 480;
+// Fixed parameters
+width    = 800;
+height   = width;
+colors   = d3.scale.category20b();
+duration = 200;
+style    = "position: absolute;\
+            text-align: center;\
+            width: 130px;\
+            height: 17px;\
+            padding: 8px;\
+            font: 14px sans-serif;\
+            background: #ddd;\
+            border: solid 1px #aaa;\
+            border-radius: 2px;\
+            pointer-events: none;";
 
-var projection = d3.geo.mercator();
-
-var path = d3.geo.path().projection(projection);
+// Creates projection
+var projection = d3.geo.mercator()
+        .scale(1)
+        .translate([0, 0]);
 
 d3.json("content/data.json", function (error, data) {
-    if (error) return console.error(error);
+    if(error) return console.error(error);
 
+    // Add SVG to the DOM
     var svg = d3.select("#map").append("svg")
-                                .attr("width", width)
-                                .attr("height", height);
+                .attr("width", width)
+                .attr("height", height);
 
-    projection.scale(1).translate([0, 0])
+    // Creates tooltip
+    tooltip = d3.select("body").append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0)
+                    .attr("style", style);
 
+    // Create path
+    var path = d3.geo.path().projection(projection);
+
+    // Compute projection parameters and apply them
     var b = path.bounds(data),
         s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
         t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-
     projection.scale(s).translate(t);
 
     svg.selectAll("path")
         .data(data.features)
-      .enter().append("path")
-        .attr("d", path);
+        .enter().append("path")
+        .attr("d", path)
+        .style("fill", function (d) { return colors(d.properties['ID'] % 20); })
+        .on("mouseover", function (d) {
+            // Brighten the color
+            d3.select(this).transition()
+                .duration(duration)
+                .style("fill", function () {
+                    return d3.rgb(colors(d.properties['ID'] % 20)).brighter(0.3);
+                });
 
-});
+            showTooltip(d.properties['name']);
+        })
+        .on("mousemove", moveTooltip)
+        .on("mouseout", function (d) {
+            // Put the color back in its default state
+            d3.select(this).transition()
+                .duration(duration)
+                .style("fill", function () {
+                    return colors(d.properties['ID'] % 20);
+                });
+
+            hideTooltip();
+        })
+        .on("click", function (d) {
+            // Redirect to the country page
+            document.location.href = "country.php?id=" + d.properties['ID'];
+        });
+
+
+    function showTooltip(name) {
+        tooltip
+            .text(name)
+            .transition()
+            .duration(duration)
+            .style("opacity", "0.9");
+    }
+
+    function moveTooltip() {
+        tooltip
+            .style({
+                "left"  : (d3.event.pageX - 130) + "px",
+                "top"   : (d3.event.pageY - 50) + "px"});
+    }
+
+    function hideTooltip() {
+        tooltip
+            .transition()
+            .duration(duration)
+            .style("opacity", "0");
+    }
+
+})
