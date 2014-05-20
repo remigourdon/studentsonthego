71571bd563b750009ffd23f71c942d0b6549006b
remigourdon <contact@remigourdon.io>
2014-05-20 16:44:58 +0200

    Pie charts fundations.

--- js/form.js
+++ js/form.js
@@ -1,8 +1,47 @@
 /**
- * Dynamic form to calculate an estimation of the price of the stay
+ * Dynamic form to calculate an estimation of the price of the stay.
+ *
+ * Display the result both as text and pie chart.
  */
 
 $(function () {
+
+    // Fixed parameters
+    var width = $("#map").width(),
+        aspect = 800 / 800,
+        height = width * aspect
+        radius = Math.min(width, height) / 2;
+
+    //
+    // Prepare the graph
+    //
+
+    // Add SVG to the DOM
+    var svg = d3.select("#resultGraph").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .attr("viewBox", "0 0 " + width + " " + height)
+                .attr("preserveAspectRatio", "xMidYMid");
+
+    // Dynamic resizingt of the svg
+    $(window).resize(function() {
+      var width = $("#map").width();
+      svg.attr("width", width);
+      svg.attr("height", width * aspect);
+    });
+
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(20);
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.value; });
+
+
 
     // Get json data
     var file = "content/json/country_" + getUrlValue("id") + ".json";
@@ -31,20 +70,32 @@ $(function () {
                 "transport" : $('#transportYes').prop("checked") ? 1 : 0,
                 "internet"  : $('#internetYes').prop("checked") ? 1 : 0,
                 "duration"  : parseFloat($('#durationStay').val())
-            }
+            };
 
-            var categories = {
-                "food"          :   inputs["nbFastfood"]  * parseFloat(prices['fastfood']) +
-                                    inputs["nbBeer"]      * parseFloat(prices['beer']),
-                "entertainment" :   inputs["nbCinema"]    * parseFloat(prices['cinema']) +
-                                    inputs["fitness"]     * parseFloat(prices['fitness']),
-                "various"       :   inputs["internet"]    * parseFloat(prices['internet']),
-                "housing"       :   parseFloat(prices['rent'])
-            }
+            var categories = [
+                {
+                    "label":    "Food",
+                    "value":    inputs["nbFastfood"]  * parseFloat(prices['fastfood']) +
+                                inputs["nbBeer"]      * parseFloat(prices['beer'])
+                },
+                {
+                    "label":    "Entertainment",
+                    "value":    inputs["nbCinema"]    * parseFloat(prices['cinema']) +
+                                inputs["fitness"]     * parseFloat(prices['fitness'])
+                },
+                {
+                    "label":    "Housing",
+                    "value":    parseFloat(prices['rent'])
+                },
+                {
+                    "label":    "Various",
+                    "value":    inputs["internet"]    * parseFloat(prices['internet'])
+                }
+            ];
 
             var result = 0;
 
-            $.each(categories, function (k, v) { result += v; });
+            categories.forEach(function(d) { result += d.value; });
 
             result = !isNaN(result * inputs['duration']) ? result * inputs['duration'] : 0;
 
@@ -55,6 +106,19 @@ $(function () {
             // console.log(result);
 
             $('#result').text("Total: " + result + "$");
+
+            plot(categories);
+        }
+
+        function plot(categories) {
+            var g = svg.selectAll(".arc")
+                .data(pie(categories))
+                .enter().append("g")
+                .attr("class", "arc");
+
+            g.append("path")
+              .attr("d", arc)
+              .style("fill", function(d, i) { return color(i); });
         }
 
     });
