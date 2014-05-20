0ce1c3a2fdddb747bd597c1df8b14043cdfd1758
remigourdon <contact@remigourdon.io>
2014-05-20 23:32:32 +0200

    Modify script link in HTMLTemplate.

--- inc/HTMLTemplate.php
+++ inc/HTMLTemplate.php
@@ -201,7 +201,7 @@ function footer($map = "") {
     <!-- ===============Bootstrap core JavaScript================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery-2.1.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <script src="js/d3.min.js"></script>
     <script src="js/{$map}-map.js"></script>
